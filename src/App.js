eed11ddfe22c9edebe534ebce40d4fb617ed7595
malloryhaigh <mallory.haigh@gmail.com>
2019-02-28 22:52:20 -0500

    Added more options

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// Set some functions
 function FeedButton(props) {
   return (
       <button onClick={props.onClick} className="btn btn-primary">
@@ -25,7 +26,45 @@ function SuperFull(props) {
   );
 }
 
-class ThisHorse extends Component {
+function ActionRide(props) {
+  return(
+      <button onClick={props.onClick} className="btn btn-primary">
+        Ride Your Pony
+      </button>
+  );
+}
+
+function ActionFeed(props) {
+  return(
+      <button onClick={props.onClick} className="btn btn-primary">
+        Feed Your Pony
+      </button>
+  );
+}
+
+function ActionOptions(props) {
+  return(
+      <button onClick={props.onClick} className="btn btn-primary">
+        Back To Options
+      </button>
+  );
+}
+
+
+// RIDE YOUR PONY
+class RideHorse extends Component {
+  render() {
+    return(
+        <div align="center" class="jumbotron">
+          <h3>Lets take your pony for a ride!</h3>
+          Neigh
+        </div>
+    );
+  }
+}
+
+// FEED YOUR PONY
+class FeedHorse extends Component {
   constructor (props) {
     super(props);
     this.state = {
@@ -68,21 +107,63 @@ class ThisHorse extends Component {
           {button}<br/><br/>
           {horseImage}
         </div>
-    )
+    );
   }
 }
 
+// Select what action you want to take - feed your pony, or ride your pony!
 class Horsify extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      action: false
+    };
+  }
+
+  handleRideButton() {
+    this.setState({action: "Ride"});
+  }
+
+  handleFeedButton() {
+    this.setState({action: "Feed"});
+  }
+
+  handleBackButton() {
+    this.setState({action: false});
+  }
+
   render() {
-    return (
-        <div>
-          <nav class="navbar-dark bg-dark navpadding">
-            <a className="navbar-brand" href="#"><strong>Hors</strong>ify</a>
-          </nav>
-          <br/><br/>
-          <ThisHorse/>
-        </div>
-    );
+
+    var buttonRide = <ActionRide onClick={() => this.handleRideButton()} />;
+    var buttonFeed = <ActionFeed onClick={() => this.handleFeedButton()} />;
+    var buttonBack = <ActionOptions onClick={() => this.handleBackButton()} />;
+    if (this.state.action === false) {
+      return(
+          <div align="center" className="jumbotron">
+            <h3>What do you want to do with your pony today?</h3>
+            {buttonRide}&nbsp;&nbsp;
+            {buttonFeed}
+          </div>
+      );
+    }
+
+    if (this.state.action === "Ride") {
+      return (
+          <div align="center">
+            <RideHorse/><br/><br/>
+            {buttonBack}
+          </div>
+      );
+    }
+
+    if (this.state.action === "Feed") {
+      return (
+          <div align="center">
+            <FeedHorse/><br/><br/>
+            {buttonBack}
+          </div>
+      );
+    }
   }
 }
 
